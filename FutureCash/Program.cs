51fb0885021df9404d85c743e2fe249d0644ca89
Walter Rothbard <walter.rothbard@gmail.com>
2018-12-27 11:21:08 -0600

    Initial import

---
+++ FutureCash/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FutureCash
+{
+    class Program
+    {
+        class Hasher : IDisposable
+        {
+            private SHA256 sha256 = SHA256.Create();
+
+            protected virtual void Dispose(bool disposing)
+            {
+                if (disposing)
+                {
+                    sha256.Dispose();
+                }
+            }
+
+            public void Dispose()
+            {
+                Dispose(true);
+            }
+
+            public byte[] Sha256d(byte[] buffer)
+            {
+                return sha256.ComputeHash(sha256.ComputeHash(buffer));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var payload = "XXXXX";
+            var buffer = Encoding.UTF8.GetBytes(payload);
+            using (var hasher = new Hasher())
+            {
+                var hash = hasher.Sha256d(buffer);
+                Console.WriteLine(BitConverter.ToString(hash));
+            }
+            Console.ReadKey();
+        }
+    }
+}
